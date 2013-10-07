ef9361a67e9808075777d6bbd516414d02fb218b
Frederic Bour <frederic.bour@lakaban.net>
2013-10-07 23:10:03 +0200

    Update for last ocp-indent

--- autoload/ocpindent.py
+++ autoload/ocpindent.py
@@ -2,137 +2,30 @@ import os
 import sys
 import vim
 import subprocess
-import bisect
 
-import vimbufsync
-vimbufsync.check_version("0.1.0", who="ocp-indent-vim")
-
-inline_process = None
-saved_states = []
-saved_sync = None
 ocp_indent_path = "ocp-indent"
 
-insert_mode = False
-equal_line = None
-equal_consecutive = 0
-equal_lines = []
-
-def sync():
-  global saved_sync, saved_states
-  curr_sync = vimbufsync.sync()
-  if not saved_sync or curr_sync.buf() != saved_sync.buf():
-    saved_states = []
-  else:
-    line = saved_sync.line()
-    i = bisect.bisect_left(saved_states, (line,""))
-    if i != len(saved_states):
-      saved_states = saved_states[:i]
-  saved_sync = curr_sync
-
-#########################################################
-# Process management, not really related to indentation #
-#########################################################
-
-def ocp_indent(content,state=None,lines=None):
-  global ocp_indent_path
+def ocp_indent(lines):
   if lines:
     if type(lines) == int:
+      end = lines
       lines = "%d-%d" % (lines,lines)
     else:
+      end = lines[1]
       lines = "%d-%d" % lines
-  else:
-    lines = "0-0"
-  if not state:
-    state = ""
-  content = "\n".join([state] + content + [""])
+  content = "\n".join(vim.current.buffer[:end] + [""])
   process = subprocess.Popen(
-      [ocp_indent_path,"--lines",lines,"--numeric","--marshal-state"],
+      [ocp_indent_path,"--lines",lines,"--numeric"],
       stdin=subprocess.PIPE, stdout=subprocess.PIPE)
   process.stdin.write(content)
   process.stdin.close()
   answer = process.stdout.readlines()
   state = answer.pop()
-  [line,col] = map(int,state.split(",")[:2])
-  return ((line-1,col,state),answer)
-
-########################################
-# Start from here to tweak indentation #
-########################################
-
-def emptyline(s):
-  return s == "" or s.isspace()
-
-def ocpindentline(l,count=1):
-  global saved_states
-  # Drop invalid states
-  if l <= 2:
-    saved_states = []
-  else:
-    i = bisect.bisect_left(saved_states, (l-1,0,""))
-    if i != len(saved_states):
-      saved_states = saved_states[:i]
-
-  # Get or generate state to resume from
-  if saved_states:
-    l0, c0, state = saved_states[-1]
-  else:
-    l0, c0, state = 0, 0, None
-  if l - l0 > 10:
-    content = vim.current.buffer[l0:l-4]
-    ((l0,c0,state), _) = ocp_indent(content,state)
-    saved_states.append((l0,c0,state))
-
-  # Indent
-  content = list(vim.current.buffer[l0:l+count-1])
-  content[0] = content[0][c0:]
-  if count == 1 and emptyline(content[-1]):
-    content[-1] = "X"
-
-  if count == 1:
-    _, content = ocp_indent(content,state,lines=l)
-    if len(content):
-      return int(content[-1])
-    else:
-      return -1
-  else:
-    _, content = ocp_indent(content,state,lines=(l,l+count-1))
-    if len(content):
-      return list(map(int,content))
-    else:
-      return None
-
-def vim_insertenter():
-  global insert_mode, equal_line, equal_lines
-  equal_line = None
-  equal_lines = []
-  insert_mode = True
-  sync()
-
-def vim_insertleave():
-  global insert_mode
-  insert_mode = False
-  sync()
+  return int(state)
 
 def vim_indentline():
-  global insert_mode, equal_line, equal_lines, equal_consecutive
   line = int(vim.eval("v:lnum"))
-  if insert_mode or (equal_line != line and equal_consecutive < 5):
-    if equal_line != line: equal_consecutive = 0
-    else: equal_consecutive += 1
-    equal_line = line + 1
-    equal_lines = []
-    return ocpindentline(line)
-  else:
-    if equal_line != line or not equal_lines:
-      sync()
-      equal_lines = ocpindentline(line,count=500)
-      if equal_lines: equal_lines.reverse()
-
-    if equal_lines:
-      equal_line = line + 1
-      return equal_lines.pop()
-    else:
-      return -1
+  return ocp_indent(line)
 
 def vim_equal():
   r = vim.current.range
--- autoload/ocpindent.vim
+++ autoload/ocpindent.vim
@@ -2,8 +2,6 @@ let s:current_dir=expand("<sfile>:p:h")
 py import sys, vim
 py if not vim.eval("s:current_dir") in sys.path:
 \    sys.path.append(vim.eval("s:current_dir"))
-
-call vimbufsync#init()
 py import ocpindent
 
 function! ocpindent#init()
