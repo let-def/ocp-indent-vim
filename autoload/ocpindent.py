153e1352b9cfc5d112dec48c37aa15ad6d149fa5
Frederic Bour <frederic.bour@lakaban.net>
2013-11-21 13:22:13 +0100

    Tweaking region indentation heuristics and default line indentation

--- autoload/ocpindent.py
+++ autoload/ocpindent.py
@@ -9,6 +9,7 @@ ocp_lastline = None
 ocp_lasttime = None
 ocp_linefst = 0
 ocp_linebuf = []
+ocp_inarow = 0
 
 def ocp_indent(lines):
   if lines:
@@ -18,7 +19,7 @@ def ocp_indent(lines):
     else:
       end = lines[1]
       lines = "%d-%d" % lines
-  content = "\n".join(vim.current.buffer[:end] + [""])
+  content = "\n".join(vim.current.buffer[:end] + ["X"])
   process = subprocess.Popen(
       [ocp_indent_path,"--lines",lines,"--numeric"],
       stdin=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -27,19 +28,26 @@ def ocp_indent(lines):
   return map(int,process.stdout.readlines())
 
 def vim_indentline():
-  global ocp_lastline, ocp_lasttime, ocp_linefst, ocp_linebuf
+  global ocp_lastline, ocp_lasttime, ocp_linefst, ocp_linebuf, ocp_inarow
   line = int(vim.eval("v:lnum"))
   if ocp_lastline == line - 1 and abs(time.time() - ocp_lasttime) < 0.1:
-    # Possibly a selection indentation
-    if not (line >= ocp_linefst and line < ocp_linefst + len(ocp_linebuf)):
-      ocp_linefst = line
-      ocp_linebuf = ocp_indent((line, min(line + 200, len(vim.current.buffer))))
-    indent = ocp_linebuf[line - ocp_linefst]
+    # Possibly a selection indentation, use a threshold to detect consecutive calls
+    if ocp_inarow > 2:
+      if not (line >= ocp_linefst and line < ocp_linefst + len(ocp_linebuf)):
+        ocp_linefst = line
+        ocp_linebuf = ocp_indent((line, min(line + 200, len(vim.current.buffer))))
+      return ocp_linebuf[line - ocp_linefst]
+    else:
+      # Increment counter
+      ocp_inarow += 1
   else:
-    # Current line indentation
-    ocp_linebuf = []
-    indent = ocp_indent(line)
-    indent = indent.pop()
+    # Not a selection indentation
+    ocp_inarow = 0
+
+  # Current line indentation
+  ocp_linebuf = []
+  indent = ocp_indent(line)
+  indent = indent.pop()
   ocp_lasttime = time.time()
   ocp_lastline = line
   return indent
